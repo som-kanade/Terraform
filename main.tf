af411b6db25c9291cec1f515de0799293bc2f1f1
somashekhar <37308901+som-kanade@users.noreply.github.com>
2020-07-26 23:03:52 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,140 @@
+provider "aws" {
+  region  = var.region
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+# aws vpc 
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "main"
+  }
+}
+
+#aws_internet_gateway
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main"
+  }
+}
+
+# route table 
+resource "aws_route_table" "r" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "main"
+  }
+}
+
+# subnet
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Main"
+  }
+}
+
+
+#associate RT to subnet
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.r.id
+}
+
+#security_groups
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+    ingress {
+    description = "TLS from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+     ingress {
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_traffic"
+  }
+}
+
+#NIC
+resource "aws_network_interface" "test" {
+  subnet_id       = aws_subnet.main.id
+  private_ips     = ["10.0.1.50"]
+  security_groups = [aws_security_group.allow_tls.id]
+
+}
+
+#EIP
+
+resource "aws_eip" "one" {
+  vpc                       = true
+  network_interface         = aws_network_interface.test.id
+  associate_with_private_ip = "10.0.1.50"
+  depends_on = ["aws_internet_gateway.gw"]
+}
+
+resource "aws_instance" "web-server" {
+  ami = "ami-063e3af9d2cc7fe94"
+  instance_type = "t2.micro"
+  key_name = "personalaws"
+
+  network_interface {
+    device_index = 0
+    network_interface_id = aws_network_interface.test.id
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt-get update -y
+              sudo apt-get install apache2 -y 
+              sudo systemctl start apache2
+              sudo bash -c 'this is deployed using terraform > var/www/html/index.html'
+              EOF
+
+}
+
+
+
+
+
